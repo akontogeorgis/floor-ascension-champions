3198afe95256a7aefb3885be2292323bc7b418e6
akontoge <akontoge@ote.gr>
2026-04-15 10:21:35 +0300

    only personal stats

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,15 +2,20 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import Leaderboard from "@/components/Leaderboard";
 import ActivityFeed from "@/components/ActivityFeed";
+// import PersonalDashboard from "@/components/PersonalDashboard";
 import IntegrationSection from "@/components/IntegrationSection";
+// import UserStats from "@/components/UserStats";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <HeroSection />
-      <Leaderboard />
-      <ActivityFeed />
+      {/* <div className="container py-8">
+        <UserStats />
+      </div> */}
+      {/* <Leaderboard />
+      <ActivityFeed /> */}
       <IntegrationSection />
     </div>
   );
